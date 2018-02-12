dc681374381ae3cfcdfcc9cfb2d76fb3752d7c32
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2018-02-12 20:56:10 +0100

    Update Evenement.java

--- src/entities/Evenement.java
+++ src/entities/Evenement.java
@@ -20,12 +20,13 @@ public class Evenement {
   Date DATEDEBUT_E ; 
   Date DATEFIN_E ; 
   String LIEU_E	;
-  String DUREE_E ; 
+  int  DUREE_E ; 
   float FRAIS_E ; 
   String ORGANISATEUR_E ; 
   String CONTACT_E ; 
+  String TYPE_E; 
 
-    public Evenement(int ID_EVENEMENT, int ID_UTILISATEUR, String TITRE_E, String DESCRIPTION_E, String AFFICHE_E, Date DATEDEBUT_E, Date DATEFIN_E, String LIEU_E, String DUREE_E, float FRAIS_E, String ORGANISATEUR_E, String CONTACT_E) {
+    public Evenement(int ID_EVENEMENT, int ID_UTILISATEUR, String TITRE_E, String DESCRIPTION_E, String AFFICHE_E, Date DATEDEBUT_E, Date DATEFIN_E, String LIEU_E, int DUREE_E, float FRAIS_E, String ORGANISATEUR_E, String CONTACT_E, String TYPE_E) {
         this.ID_EVENEMENT = ID_EVENEMENT;
         this.ID_UTILISATEUR = ID_UTILISATEUR;
         this.TITRE_E = TITRE_E;
@@ -38,8 +39,10 @@ public class Evenement {
         this.FRAIS_E = FRAIS_E;
         this.ORGANISATEUR_E = ORGANISATEUR_E;
         this.CONTACT_E = CONTACT_E;
+        this.TYPE_E = TYPE_E;
     }
 
+    
     public int getID_EVENEMENT() {
         return ID_EVENEMENT;
     }
@@ -72,7 +75,7 @@ public class Evenement {
         return LIEU_E;
     }
 
-    public String getDUREE_E() {
+    public int getDUREE_E() {
         return DUREE_E;
     }
 
@@ -86,6 +89,10 @@ public class Evenement {
 
     public String getCONTACT_E() {
         return CONTACT_E;
+    }
+
+    public String getTYPE_E() {
+        return TYPE_E;
     }
 
     public void setID_EVENEMENT(int ID_EVENEMENT) {
@@ -120,7 +127,7 @@ public class Evenement {
         this.LIEU_E = LIEU_E;
     }
 
-    public void setDUREE_E(String DUREE_E) {
+    public void setDUREE_E(int DUREE_E) {
         this.DUREE_E = DUREE_E;
     }
 
@@ -139,6 +146,10 @@ public class Evenement {
     @Override
     public String toString() {
         return "Evenement{" + "ID_EVENEMENT=" + ID_EVENEMENT + ", ID_UTILISATEUR=" + ID_UTILISATEUR + ", TITRE_E=" + TITRE_E + ", DESCRIPTION_E=" + DESCRIPTION_E + ", AFFICHE_E=" + AFFICHE_E + ", DATEDEBUT_E=" + DATEDEBUT_E + ", DATEFIN_E=" + DATEFIN_E + ", LIEU_E=" + LIEU_E + ", DUREE_E=" + DUREE_E + ", FRAIS_E=" + FRAIS_E + ", ORGANISATEUR_E=" + ORGANISATEUR_E + ", CONTACT_E=" + CONTACT_E + '}';
+    }
+
+    public void setTYPE_E(String TYPE_E) {
+        this.TYPE_E = TYPE_E;
     }
   
   
